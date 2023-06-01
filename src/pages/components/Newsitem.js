ca9e42e1ffc0e95f56626fa21817be3c0b1dfe31
Adnane2002 <adnanedouiab8@gmail.com>
2023-06-01 22:44:17 +0100

    jnjbj

---
+++ src/pages/components/Newsitem.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Image from "";
+import styles from "../../styles/NewsItem.module.css";
+
+export default function NewsItem({ news }) {
+  return (
+    <div className={styles.news}>
+      <div className={styles.img}>
+        <Image
+          src={news.image ? news.image : "No Image"}
+          width={150}
+          height={100}
+          alt="image"
+        />
+      </div>
+      <div className={styles.info}>
+        <span>
+          {news.date} {news.time}
+        </span>
+        <h3>{news.name}</h3>
+      </div>
+      <div className={styles.link}>
+        <Link href={`/news/${news.slug}`}>
+          <div className="btn">Read More</div>
+        </Link>
+      </div>
+    </div>
+  );
+}
