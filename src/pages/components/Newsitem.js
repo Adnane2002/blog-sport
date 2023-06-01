7169a6df14e9f9b32474a320cb508685edd2db97
Adnane2002 <adnanedouiab8@gmail.com>
2023-06-01 23:14:24 +0100

    njnjn

--- src/pages/components/Newsitem.js
+++ src/pages/components/Newsitem.js
@@ -1,18 +1,9 @@
 import Link from "next/link";
 import styles from "../../styles/NewsItem.module.css";
-import Image from "next/image";
 
 export default function NewsItem({ news }) {
   return (
     <div className={styles.news}>
-      <div className={styles.img}>
-        <Image
-          src={news.image ? news.image : "No Image"}
-          width={150}
-          height={100}
-          alt="image"
-        />
-      </div>
       <div className={styles.info}>
         <span>
           {news.date} {news.time}
