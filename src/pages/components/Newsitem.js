85fbe2d62dcf325645c72a638599d165c88d8f62
Adnane2002 <adnanedouiab8@gmail.com>
2023-06-02 01:27:36 +0100

    nn

--- src/pages/components/Newsitem.js
+++ src/pages/components/Newsitem.js
@@ -2,20 +2,24 @@ import Link from "next/link";
 import styles from "../../styles/NewsItem.module.css";
 
 export default function NewsItem({ news }) {
-  const {date, time, name, slug } = news; 
+  const { date, time, name, slug } = news;
 
   return (
     <div className={styles.news}>
       <div className={styles.info}>
-        <span>
-        {date} {time}
-        </span>
-        <h3>{name}</h3>
+        {date && time && (
+          <span>
+            {date} {time}
+          </span>
+        )}
+        {name && <h3>{name}</h3>}
       </div>
       <div className={styles.link}>
-        <Link href={`/news/${slug}`}>
-          <div className="btn">Read More</div>
-        </Link>
+        {slug && (
+          <Link href={`/news/${slug}`}>
+            <div className="btn">Read More</div>
+          </Link>
+        )}
       </div>
     </div>
   );
