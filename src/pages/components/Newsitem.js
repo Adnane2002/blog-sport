68dda8f13ce4f77014578994239bf8ee565abebc
Adnane2002 <adnanedouiab8@gmail.com>
2023-06-02 01:33:01 +0100

    cccc

--- src/pages/components/Newsitem.js
+++ src/pages/components/Newsitem.js
@@ -1,26 +1,28 @@
-import Link from "next/link";
-import styles from "../../styles/NewsItem.module.css";
-
 export default function NewsItem({ news }) {
   const { date, time, name, slug } = news;
 
   return (
     <div className={styles.news}>
       <div className={styles.info}>
-        {date && time && (
+        {date != null && time != null ? (
           <span>
             {date} {time}
           </span>
+        ) : (
+          <span>No Date or Time</span>
         )}
-        {name && <h3>{name}</h3>}
+        {name ? <h3>{name}</h3> : <h3>No Name</h3>}
       </div>
       <div className={styles.link}>
-        {slug && (
+        {slug ? (
           <Link href={`/news/${slug}`}>
             <div className="btn">Read More</div>
           </Link>
+        ) : (
+          <div className="btn">No Slug</div>
         )}
       </div>
     </div>
   );
 }
+
