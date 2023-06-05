ff4116d35a5db63c5c668418b34a886e737495c0
Adnane2002 <adnanedouiab8@gmail.com>
2023-06-05 13:12:16 +0100

    cc

--- src/pages/news/[slug].js
+++ src/pages/news/[slug].js
@@ -7,48 +7,28 @@ import styles from "../../styles/News.module.css"
 export default function SingleNews({ news }) {
   const router = useRouter();
   console.log("router===>", router);
-  return (
-      <div className={styles.news}>
-        <span>
-          {news.date} {news.time}
-        </span>
 
-        <h1>{news.name}</h1>
-        {news.image && (
-          <div className={styles.image}>
-            <Image alt="image" src={news.image} width={900} height={600} />
-          </div>
-        )}
-        <p>{news.detail}</p>
-        <Link className="btn-view" href="/news">
-          Go Back
-        </Link>
-      </div>
+  // Add a conditional check to handle the case when 'news' is undefined
+  if (!news) {
+    return <div>Loading...</div>; // You can replace this with an appropriate loading indicator
+  }
+
+  return (
+    <div className={styles.news}>
+      <span>
+        {news.date} {news.time}
+      </span>
+
+      <h1>{news.name}</h1>
+      {news.image && (
+        <div className={styles.image}>
+          <Image alt="image" src={news.image} width={900} height={600} />
+        </div>
+      )}
+      <p>{news.detail}</p>
+      <Link className="btn-view" href="/news">
+        Go Back
+      </Link>
+    </div>
   );
 }
-
-export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/api/news`);
-  const news = await res.json();
-  const paths = news.map((item) => ({
-    params: { slug: item.slug },
-  }));
-
-  return {
-    paths,
-    fallback: true,
-  };
-}
-
-export async function getStaticProps({ params: { slug } }) {
-  const res = await fetch(`${API_URL}/api/news/${slug}`);
-  const singleNews = await res.json();
-  return {
-    props: {
-      news: singleNews[0],
-    },
-    revalidate: 1,
-  };
-}
-
-
